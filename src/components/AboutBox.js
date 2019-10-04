2a3354f5e0256f3be87ab8252d0f3a6c823a5b40
toomusa <3895499+toomusa@users.noreply.github.com>
2019-10-03 19:54:41 -0700

    change about box photo

--- src/components/AboutBox.js
+++ src/components/AboutBox.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import Genie from "../assets/images/Genie.png"
+import Einstein from "../assets/images/Einstein.png"
 
 export default function AboutBox(props) {
   return (
     <div id="aboutBox">
       <Link to="/about" onClick={() => props.activate("/about")} >
         <h5>About</h5>
-        <img src={Genie} alt="About" id="aboutImage" />
+        <img src={Einstein} alt="About" id="aboutImage" />
       </Link>
     </div>
   )
